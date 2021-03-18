975a9adbff87d1e0ac32f2880e9c36c8e3a5c5fb
Roza Chaikozova <r.chaikozova@gmail.com>
2021-03-18 23:08:26 +0600

    reset pswrd

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('api/logout/', user_views.LogoutView.as_view(), name='logout'),
     path('api/user-create/', user_views.UserRegistrationView.as_view(), name='user-create'),
     path('api/user/<int:pk>/', user_views.UserRetrieveUpdateDeleteAPIView.as_view(), name='user'),
+    path('api/user/', user_views.UserListView.as_view(), name='user-list'),
+    path('password-reset/', user_views.reset_user_password, name="password-reset"),
+
 ]
